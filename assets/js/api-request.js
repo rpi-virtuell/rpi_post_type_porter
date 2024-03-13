34877432ab451c0ffad935548855ec1bae9fcddf
Daniel Reintanz <85614916+FreelancerAMP@users.noreply.github.com>
2024-03-13 17:43:54 +0100

    Updated current state still WIP

--- assets/js/api-request.js
+++ assets/js/api-request.js
@@ -36,26 +36,24 @@ jQuery(document).ready(function ($) {
 
 
         var routeMapping = {"status": post_status, "type": post_type, "author": post_author,};
-        var dataFields = ['status', 'type', 'author', 'acf', 'meta','_links', 'wp:attachment' ,'wp:term', 'version_history'];
+        var dataFields = ['status', 'type', 'author', 'acf', 'meta', '_links', 'wp:attachment', 'wp:term', 'version_history'];
         for (var key in data) {
-            if (dataFields.includes(key) ||  recursiv)
-            {
+            if (dataFields.includes(key) || recursiv) {
                 if (routeMapping[key] !== undefined) {
-                    html += '<label class="'+key+'"><strong>'+key+'</strong></label></br>';
-                    html += '<select class="rpi-porter-select-box" name="'+key+'">';
-                    for (var post_data_key in routeMapping[key]){
-                        html += '<option value="'+post_data_key+'">'+routeMapping[key][post_data_key]+'</option>'
+                    html += '<label class="' + key + '"><strong>' + key + '</strong></label></br>';
+                    html += '<select class="rpi-porter-select-box" name="' + key + '">';
+                    for (var post_data_key in routeMapping[key]) {
+                        html += '<option value="' + post_data_key + '">' + routeMapping[key][post_data_key] + '</option>'
                     }
                     html += '</select></br>';
                     continue;
                 }
-                if (routeMapping[key] === undefined   && data[key] !== null && typeof data[key] === 'object') {
+                if (routeMapping[key] === undefined && data[key] !== null && typeof data[key] === 'object') {
                     html += '<div class="rpi-porter-object-container"><strong class="rpi-porter-object-title">' + key + '</strong>';
                     html += '<div class="rpi-porter-object-content">';
                     html += iterateThroughData(data[key], true);
                     html += '</div></div>';
-                }
-                else {
+                } else {
                     html += createInputRow(key, data[key]);
                 }
             }
@@ -77,38 +75,77 @@ jQuery(document).ready(function ($) {
 
     jQuery(document).ready(function ($) {
         $('#import-button').click(function () {
-            rpi-porter-progress
+            rpi - porter - progress
             $(this).find('.rpi-porter-progress').html += '<div class="progress-container">' +
                 '<div class="progress-bar" id="myProgressBar"></div>' +
                 '    </div>' +
                 '    <button id="stopButton">Stop Import</button>\n';
-            var postMapping = collectDataForImport();
+            var postMapping = collectDataForImport(); // todo this needs to return all fields which need to be imported
 
-
+            preparePostsForImport();
+            //TODO get post count. Iterate thorugh data array and unset all data which is not mapped
+            //   cluster posts in 5 and send to server. after received answer
             //TODO: this part needs to be in its own function to be called recursivly
             // required params are next route and field_template
             // optional param is created posts
-            $.ajax({
-                url: ajaxurl,
-                type: 'POST',
-                data: {
-                    action: 'rpi_porter_save_post',
-                    postMapping: postMapping,
-                    nonce: my_ajax_obj.nonce // Stellen Sie sicher, dass die Nonce übergeben wird
-                },
-                success: function (response) {
-                    alert('Import erfolgreich!');
-                    updateProgressBar();
-                    //TODO ADD update Progress bar logic
-                    //TODO: check if stop  button has been pressed
-                    // if not recall save post function with passed data from response
-                },
-                error: function () {
-                    alert('Fehler beim Import.');
-                }
-            });
+
         });
     });
+
+    function preparePostsForImport(posts, mapping) {
+        /*
+        this checks if the key in post are present in mapping if not it is removed
+         from the array. if the Key is present but the value of the mapping is not
+         the same it is replaced by the mapping key and the old key is removed
+         */
+
+        posts.forEach(function (item, index, arr) {
+            if (!index in mapping) {
+                arr.splice(index, 1);
+            } else {
+                if (mapping[index] !== index) {
+                    arr[index] = arr[mapping[index]];
+                    arr.splice(index, 1);
+
+
+                }
+
+            }
+        });
+
+    }
+
+    function send_prepared_posts_via_ajax() {
+        if (data.length === totalposts) {
+
+            console.log('Import abgeschlossen')
+            return;
+        }
+        $.ajax({
+            url: ajaxurl,
+            type: 'POST',
+            data: {
+                action: 'rpi_porter_save_post',
+                postMapping: postMapping,
+                nonce: my_ajax_obj.nonce // Stellen Sie sicher, dass die Nonce übergeben wird
+            },
+            success: function (response) {
+                alert('Import erfolgreich!');
+                updateProgressBar();
+
+                //TODO ADD update Progress bar logic
+                //TODO: check if stop  button has been pressed
+                // if not recall save post function with passed data from response
+            },
+            error: function () {
+                alert('Fehler beim Import.');
+            },
+            complete: function () {
+                send_prepared_posts_via_ajax();
+            }
+        });
+
+    }
 
     function updateProgressBar() {
         const progress = (importedData / totalData) * 100;
@@ -133,7 +170,7 @@ jQuery(document).ready(function ($) {
                 if ($(this).find('.rpi-porter-import-check').is(':checked')) {
                     postMapping[$(this).find('.rpi-porter-old-key').val()] = $(this).find('.rpi-porter-new-key').val();
                 }
-                if ($(this).find('.rpi-porter-select-box').length){
+                if ($(this).find('.rpi-porter-select-box').length) {
                     postMapping[$(this).find('.rpi-porter-select-box').name] = $(this).find('.rpi-porter-select-box').val();
                 }
             });
--- rpiposttypeporter.php
+++ rpiposttypeporter.php
@@ -1,12 +1,12 @@
 <?php
-/*
-Plugin Name: RPI Export-Import
-Plugin URI: http://beispiel.de/plugins/places-export-import-plugin
-Description: Exportiert und importiert einen Beitragstyp inklusive ACF-Felder zwischen WordPress-Instanzen und wandelt Google Maps Koordinaten in Leaflet JS Koordinaten um.
-Version: 1.0
-Author: Daniel Reintanz
-License: GPLv2
-*/
+/**
+ * Plugin Name: RPI Export-Import
+ * Plugin URI: http://beispiel.de/plugins/places-export-import-plugin
+ * Description: Exportiert und importiert einen Beitragstyp inklusive ACF-Felder zwischen WordPress-Instanzen und wandelt Google Maps Koordinaten in Leaflet JS Koordinaten um.
+ * Version: 1.0
+ * Author: Daniel Reintanz
+ * License: GPLv2
+ */
 
 // Sicherstellen, dass das Plugin nicht direkt aufgerufen wird
 defined('ABSPATH') or die('Direkter Zugriff auf Skripte ist nicht erlaubt.');
@@ -26,17 +26,15 @@ class RpiPostTypePorter
         wp_enqueue_script('jquery-ui-accordion');
         add_action('admin_enqueue_scripts', 'enqueue_custom_admin_script');
 
-        function enqueue_custom_admin_script() {
-
-
+        function enqueue_custom_admin_script()
+        {
             wp_enqueue_script('dynamic_fields', plugin_dir_url(__FILE__) . 'assets/js/dynamic-fields.js', array('jquery'), null, true);
             wp_enqueue_script('rpi-porter-api-request', plugin_dir_url(__FILE__) . 'assets/js/api-request.js', array('jquery'), null, true);
 
-            $post_types =get_post_types();
-            $post_status  = get_post_statuses();
+            $post_types = get_post_types();
+            $post_status = get_post_statuses();
             $post_authors = get_users();
-            foreach ($post_authors as $author)
-            {
+            foreach ($post_authors as $author) {
                 $post_authors_arr[$author->ID] = $author->user_login;
             }
             wp_localize_script('rpi-porter-api-request', 'post_type', $post_types);
@@ -50,4 +48,5 @@ class RpiPostTypePorter
     }
 
 }
+
 new RpiPostTypePorter();
